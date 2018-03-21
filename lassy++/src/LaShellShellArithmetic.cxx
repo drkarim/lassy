d0080578ce10f05d975bdc0b4def84784f9dae6f
Rashed karim <rashed.karim@gmail.com>
2018-03-21 11:36:58 +0000

    new shell scaler

--- lassy++/src/LaShellShellArithmetic.cxx
+++ lassy++/src/LaShellShellArithmetic.cxx
@@ -13,6 +13,8 @@ LaShellShellArithmetic::LaShellShellArithmetic()
 	_output_la = new LaShell();
 	_operation = ADD;
 	_factor = 1.0; 
+	_single_shell = false; 
+	_single_shell_value = 0;
 }
 
 LaShellShellArithmetic::~LaShellShellArithmetic() {
@@ -28,6 +30,15 @@ void LaShellShellArithmetic::SetInputData2(LaShell* shell) {
 	_source_la_2 = shell; 
 }
 
+void LaShellShellArithmetic::SetToSingleShellMode()
+{
+	_single_shell = true;
+}
+
+void  LaShellShellArithmetic::SetSingleShellModeValue(double val) 
+{
+	_single_shell_value = val; 
+}
 
 LaShell* LaShellShellArithmetic::GetOutput() {
 	return _output_la;
@@ -64,29 +75,41 @@ void LaShellShellArithmetic::Update() {
 		outputWindow->SetInstance(fileOutputWindow);
 	}
 
+	vtkSmartPointer<vtkPolyData> Output_Poly = vtkSmartPointer<vtkPolyData>::New();
+	
 	vtkSmartPointer<vtkPolyData> Source_Poly1 = vtkSmartPointer<vtkPolyData>::New();
 	vtkSmartPointer<vtkPolyData> Source_Poly2 = vtkSmartPointer<vtkPolyData>::New();
-	vtkSmartPointer<vtkPolyData> Output_Poly = vtkSmartPointer<vtkPolyData>::New();
-
 	vtkSmartPointer<vtkFloatArray> Scalars_Poly1 = vtkSmartPointer<vtkFloatArray>::New();
 	vtkSmartPointer<vtkFloatArray> Scalars_Poly2 = vtkSmartPointer<vtkFloatArray>::New();
-
 	_source_la_1->GetMesh3D(Source_Poly1);
-	_source_la_2->GetMesh3D(Source_Poly2);
-	
+
+
 	vtkSmartPointer<vtkFloatArray> Output_Poly_Scalar = vtkSmartPointer<vtkFloatArray>::New();
 	Output_Poly_Scalar->SetNumberOfComponents(1);
 	Output_Poly->DeepCopy(Source_Poly1);
 
 	Scalars_Poly1 = vtkFloatArray::SafeDownCast(Source_Poly1->GetPointData()->GetScalars()); 
-	Scalars_Poly2 = vtkFloatArray::SafeDownCast(Source_Poly2->GetPointData()->GetScalars());
 
+	if (!_single_shell) {
+		cout << "Two shells mode \n\n";
+		_source_la_2->GetMesh3D(Source_Poly2);
+		Scalars_Poly2 = vtkFloatArray::SafeDownCast(Source_Poly2->GetPointData()->GetScalars());
+	}
+	else {
+		cout << "Single shell model\n\n";
+	}
 
+	double scalar2;
 
 	for (vtkIdType i = 0; i < Source_Poly1->GetNumberOfPoints(); ++i) {
 
 		double scalar1 = Scalars_Poly1->GetTuple1(i);
-		double scalar2 = Scalars_Poly2->GetTuple1(i);
+		if (!_single_shell) { 
+			 scalar2 = Scalars_Poly2->GetTuple1(i);
+		}
+		else 
+			scalar2 = _single_shell_value; 
+
 		double result = 0.0; 
 
 		switch (_operation)
