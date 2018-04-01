3c3d38c334d89f88e5bbb4cdb1a2b79546f788dd
Rashed karim <rashed.karim@gmail.com>
2018-04-01 11:32:07 +0100

    New shell mask

---
+++ applications/maskshell.cxx
@@ -0,0 +1,88 @@
+#define HAS_VTK 1
+
+#include "LaMaskDataShellOperations.h"
+#include <numeric> 
+
+/*
+*      Author:
+*      Dr. Rashed Karim
+*      Department of Biomedical Engineering, King's College London
+*      Email: rashed 'dot' karim @kcl.ac.uk
+*      Copyright (c) 2017
+*
+*	   This application masks the scalars in a shell, and provides an aggregated output
+*      It is useful for performing templated analysis on atria
+*/
+int main(int argc, char * argv[])
+{
+	char* input_f1, *input_f2, *output_f;
+	int operation = MEAN;
+	bool foundArgs1 = false, foundArgs2 = false, foundArgs3 = false, foundArgs4=false;
+
+
+	if (argc >= 1)
+	{
+		for (int i = 1; i < argc; i++) {
+			if (i + 1 != argc) {
+				if (string(argv[i]) == "-d") {
+					input_f1 = argv[i + 1];
+					foundArgs1 = true;
+				}
+				else if (string(argv[i]) == "-m") {
+					input_f2 = argv[i + 1];
+					foundArgs2 = true;
+				}
+
+				else if (string(argv[i]) == "-o") {
+					output_f = argv[i + 1];
+					foundArgs3 = true;
+				}
+				else if (string(argv[i]) == "-a") {
+					operation = atoi(argv[i + 1]);
+					foundArgs4 = true;
+				}
+
+			}
+			
+		}
+	}
+
+	if (!(foundArgs1 && foundArgs2 && foundArgs4))
+	{
+		cerr << "Aggregates scalars masked in VTK mesh\nCheck your parameters\n\nUsage:"
+			"\n(Mandatory)\n\t-d <1st Scalar data VTK mesh> \n\t-m <Mask data VTK mesh>"
+			"\n\t-a <which aggregate method: 1-MEAN, more soon>\n"
+			"\n\n(Optional)"
+            "\n\t-o <output_vtk>\n";
+			
+
+
+		exit(1);
+	}
+	else
+	{
+		LaShell* source1 = new LaShell(input_f1);       // DATA SHELL
+		LaShell* source2 = new LaShell(input_f2);       // MASK SHELL
+		LaShell* la_out = new LaShell();
+
+		LaMaskDataShellOperations* algorithm = new LaMaskDataShellOperations();
+		algorithm->SetInputData(source1);
+		algorithm->SetInputData2(source2);      
+		
+		switch (operation)
+		{
+		    case MEAN:
+                algorithm->SetOperationToMean();
+                algorithm->Update();
+                cout << "Mean = " << algorithm->GetOutputValue() << endl;
+                break;
+
+		}
+
+        if (foundArgs3) {
+            la_out = algorithm->GetOutput();
+		    la_out->ExportVTK(output_f);
+        }
+	}
+
+}
---
+++ lassy++/include/LaMaskDataShellOperations.h
@@ -0,0 +1,52 @@
+/*
+*      Author:
+*      Dr. Rashed Karim
+*      Department of Biomedical Engineering, King's College London
+*      Email: rashed 'dot' karim @kcl.ac.uk
+*      Copyright (c) 2017
+*/
+
+/*
+*	The LaMaskDataShellOperations takes two mesh, the cumulative scalar of the shell covered by the mask 
+*   is returned
+*/
+#pragma once
+
+#define MEAN 1 
+
+#include "LaShellAlgorithms.h"
+#include "vtkPointLocator.h"
+#include "LaShell.h"
+#include "MathBox.h"
+
+
+
+class LaMaskDataShellOperations : public LaShellAlgorithms {
+
+private: 
+	LaShell* _data_shell; 
+	LaShell* _mask_shell;
+	
+	LaShell* _output_la;
+	int _which_operation; 
+	double _scalar_aggregate; 
+
+public:
+	// Constructor with default values for data members
+	/*static LaShellAlgorithms *New();*/
+	
+	void SetInputData(LaShell* data); 
+	void SetInputData2(LaShell* mask);
+
+	void Update(); 
+
+	double GetOutputValue();
+	LaShell* GetOutput();
+	
+	void SetOperationToMean();
+	
+
+	LaMaskDataShellOperations();
+	~LaMaskDataShellOperations();
+	
+}; 
